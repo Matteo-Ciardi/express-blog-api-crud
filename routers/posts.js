9850015dd4fccf242adafadfcc49dbdc07d1a567
MatteoCiardi <ciardimatteo99@gmail.com>
2025-10-17 12:57:25 +0200

    modificato router

--- routers/posts.js
+++ routers/posts.js
@@ -2,28 +2,10 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/', function (req, res) {
-    res.send('Lista dei post');
-})
-
-router.get('/:id', function (req, res) {
-    res.send('Dettagli dei post ' + req.params.id);
-})
-
-router.post('/', function (req, res) {
-    res.send('Creazione nuovo post');
-})
-
-router.put('/:id', function (req, res) {
-    res.send('Modifica integrale del post ' + req.params.id);
-})
-
-router.patch('/:id', function (req, res) {
-    res.send('Modifica parziale del post ' + req.params.id);
-})
-
-router.delete('/:id', function (req, res) {
-    res.send('Eliminazione post ' + req.params.id);
-})
+router.get('/', postController.index);
+router.get('/:id', postController.show);
+router.post('/', postController.store);
+router.put('/:id', postController.update);
+router.delete('/:id', postController.destroy);
 
 module.exports = router;
