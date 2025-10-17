e230b6895f2d59388fdbc34fd95f9ec9cf06f895
MatteoCiardi <ciardimatteo99@gmail.com>
2025-10-17 13:31:15 +0200

    modificato controller per milestone 2

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,23 +1,62 @@
-let posts = ('../data/postContent');
+let posts = ('../data/postsContent.js');
 
-const index = (req, res) => {
-    res.send('Lista di tutti i post')
+function index(req, res) {
+    let filteredPosts = posts;
+
+    if (req.query.tags) {
+        filteredPosts = posts.filter(
+            post => pizza.tags.includes(req.query.tags)
+        );
+    }
+
+    res.json(filteredPosts);
 }
 
-const show = (req, res) => {
-    res.send(`Post con id ${req.params.id}`);
+function show(req, res) {
+    const id = parseInt(req.params.id);
+    const post = posts.find(p => p.id === id);
+
+    if (!post) {
+        res.status(404)
+
+        return res.json({
+            error: "Post non trovato",
+            message: `Il post con id ${id} non esiste`
+        });
+    }
+
+    res.json(post);
 }
 
-const store = (req, res) => {
+function store(req, res) {
     res.send('Crea un nuovo post');
 }
 
-const update = (req, res) => {
+function update(req, res) {
     res.send(`Aggiorna post con id ${req.params.id}`);
 }
 
-const destroy = (req, res) => {
-    res.send(`Elimina post con id ${req.params.id}`);
+function modify(req, res) {
+    res.send('Modifica parziale del post ' + req.params.id);
 }
 
-module.exports = { index, show, store, update, destroy };
+function destroy(req, res) {
+    const id = parseInt(req.params.id);
+    const post = posts.findIndex(p => p.id === id);
+
+    if (!post) {
+        res.status(404);
+
+        return req.json({
+            status: 404,
+            error: "Post non trovato",
+            message: `Il post con id ${id} non esiste`
+        })
+    }
+
+    posts.splice(posts.indexOf(post), 1);
+    console.log(posts);
+    res.sendStatus(204);
+}
+
+module.exports = { index, show, store, update, modify, destroy };
--- routers/posts.js
+++ routers/posts.js
@@ -1,6 +1,6 @@
 const express = require('express');
-
 const router = express.Router();
+const postController = require('../controllers/postController');
 
 router.get('/', postController.index);
 router.get('/:id', postController.show);
